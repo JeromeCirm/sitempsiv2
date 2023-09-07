000cd5cf5b02bcdc39aa43c04fc3ed04da2897cd
JeromeCirm <jerome.nizon@gmail.com>
2023-09-07 18:16:43 +0200

    Update admin.py

--- base/admin.py
+++ base/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import *
 
 class UtilisateurAdmin(admin.ModelAdmin):
-    list_display=('user','date_demande','en_attente_confirmation','reinitialisation_password')
+    list_display=('user','date_demande','en_attente_confirmation','reinitialisation_password','doit_changer_mdp')
 
 admin.site.register(Utilisateur,UtilisateurAdmin)
